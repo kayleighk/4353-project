9c216220120419b5b61b2113b3e4ad78856503ad
BSAL2770 <78518356+BSAL2770@users.noreply.github.com>
2022-02-19 02:38:07 -0600

    Update settings.py

--- mydjango/settings.py
+++ mydjango/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i0y9+_db@%&i6s3-jqc=^mz=*@rt(4l7xcv2bgcnsww31oy+2#'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
